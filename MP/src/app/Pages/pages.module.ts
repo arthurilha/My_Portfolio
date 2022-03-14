d17ce96768ec1a299f27fbd49078444278ba975d
Arthur Battistel Ilha <73798535+arthurilha@users.noreply.github.com>
2022-03-14 16:11:26 -0300

    finish

--- MP/src/app/Pages/pages.module.ts
+++ MP/src/app/Pages/pages.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RoutingModule } from './pages-routing.module';
-import { SobreComponent } from './sobre/sobre.component';
+import { SobreComponent } from './portfolio/sobre/sobre.component';
 import { ConhecimentosComponent } from './conhecimentos/conhecimentos.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContatosComponent } from './contatos/contatos.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    SobreComponent,
     ConhecimentosComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ContatosComponent,
+    SobreComponent
+
   ],
   imports: [
     CommonModule,
